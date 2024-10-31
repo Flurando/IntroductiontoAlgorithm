3d2d67eb057f6b65f4332878456b2d827a0e4878
Jack <2024110144@mails.szu.edu.cn>
2024-10-31 22:41:47 +0800

    start

---
+++ Chapter2/2.1/2.1-2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char *argv[])
+{
+  int n = argc - 1;
+  int A[argc-1];
+  for (int index = 0; index <= argc - 2; index++)
+    {
+      A[index] = atoi (argv[index + 1]);
+    }
+  
+  for (int index = n-2; index >= 0; index--)
+    {
+      int key = A[index];
+      int index2 = index + 1;
+      while (index2 <= n - 1 && A[index2] > key)
+        {
+          A[index2-1] = A[index2];
+          index2++;
+        }
+      A[index2-1] = key;
+    }
+  
+  for (int index = 0; index <= n - 1; index++)
+    {
+      printf ("%d ", A[index]);
+    }
+  printf ("\n");
+  
+  return 0;
+}
